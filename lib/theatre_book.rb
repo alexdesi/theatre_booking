8bfa23ffd6d36c98276f93f7fa2989bae4d023ef
Alexdesi <alexdesi@gmail.com>
2020-09-19 23:51:24 +0100

    Add logic to recognize free seats and count the single gaps in a row.

--- lib/theatre_book.rb
+++ lib/theatre_book.rb
@@ -1,22 +1,68 @@
 class TheatreBook
-  ROW_MAX = 100
-  SEAT_MAX = 50
+  ROWS_NUMBER = 100
+  COLS_NUMBER = 50
 
   def initialize
-    @seats = [1..ROW_MAX][1..SEAT_MAX]
+    @seats = Array.new(ROWS_NUMBER) { Array.new(COLS_NUMBER, false) }
   end
 
-  def book(row_number, seat_number)
-    raise('The seat is not valid.') unless (1..ROW_MAX).include?(row_number) && (1..SEAT_MAX).include?(seat_number)
+  def book(row_number:, first_seat:, last_seat:)
+    unless (0..max_row).include?(row_number) &&
+           (0..max_col).include?(first_seat) &&
+           (0..max_col).include?(last_seat)
+      raise('The seat is not valid.')
+    end
 
-    if seats[row_number, seat_number]
-      raise('Seat already booked.')
+    if free_seats?(row_number, first_seat, last_seat) && single_gaps_count(row_number, first_seat, last_seat).zero?
+      (first_seat..last_seat).each do |col|
+        seats[row_number][col] = true
+      end
+
+      true
     else
-      seats[row_number, seat_number] = true
+      false
     end
   end
 
   private
+
+  def free_seats?(row_number, first_seat, last_seat)
+    seats_to_book = (first_seat..last_seat).map { |col| @seats[row_number][col] }
+
+    seats_to_book.none?
+  end
+
+  def single_gaps_count(row_number, first_seat, last_seat)
+    row_seats = @seats[row_number].clone
+
+    (first_seat..last_seat).each do |col|
+      row_seats[col] = true
+    end
+
+    gaps = 0
+    i = 1
+
+    # Exclude the first and last seats
+    while i <= max_col - 1
+      current_seat = row_seats[i]
+      previous_seat = row_seats[i - 1]
+      next_seat = row_seats[i + 1]
+
+      gaps += 1 if previous_seat && !current_seat && next_seat
+
+      i += 1
+    end
+
+    gaps
+  end
+
+  def max_row
+    ROWS_NUMBER - 1
+  end
+
+  def max_col
+    COLS_NUMBER - 1
+  end
 
   attr_accessor :seats
 end
--- spec/theatre_book_spec.rb
+++ spec/theatre_book_spec.rb
@@ -4,23 +4,45 @@ RSpec.describe TheatreBook do
   describe 'book a seat' do
     subject(:theatre_book) { described_class.new }
 
-    context 'when seat is available' do
-      it 'reserves the seat' do
-        expect(theatre_book.book(1, 3)).to be true
+    context 'when seats are available' do
+      it 'reserves the seats' do
+        expect(theatre_book.book(row_number: 1, first_seat: 3, last_seat: 6)).to be true
       end
     end
 
-    context 'when seat is already booked' do
+    context 'when at least a seat is already booked' do
       it 'does NOT reserves the seat' do
-        theatre_book.book(1, 3)
+        theatre_book.book(row_number: 1, first_seat: 3, last_seat: 6)
 
-        expect { theatre_book.book(1, 3)}.to raise_error('Seat already booked.')
+        expect(
+          theatre_book.book(row_number: 1, first_seat: 6, last_seat: 7) # Seat 6 is alreay booked
+        ).to be false
+      end
+    end
+
+    context 'when 2 bookings are consecutive' do
+      it 'does reserves the seat' do
+        theatre_book.book(row_number: 1, first_seat: 3, last_seat: 6)
+
+        expect(
+          theatre_book.book(row_number: 1, first_seat: 7, last_seat: 8)
+        ).to be true
+      end
+    end
+
+    context 'when 2 bookings have a single gap' do
+      it 'does now allow to book the seats' do
+        theatre_book.book(row_number: 1, first_seat: 3, last_seat: 6)
+
+        expect(
+          theatre_book.book(row_number: 1, first_seat: 8, last_seat: 9)
+        ).to be false
       end
     end
 
     context 'when the row OR seat number is invalid' do
       it 'raises an error' do
-        theatre_book.book(51, 1)
+        theatre_book.book(row_number: 51, first_seat: 1, last_seat: 3)
       end
     end
   end
